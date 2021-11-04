ad9b868bd286d6aaf6fff9cb1bbf800730ab16d7
Jakub Kacalek <kacalek.kuba@post.cz>
2021-11-04 16:51:33 +0100

    get public

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'));
+app.use(express.static('../public'));
 
 // app.get('/', (req, res) => {
 //   //res.send('HelloWorld!')

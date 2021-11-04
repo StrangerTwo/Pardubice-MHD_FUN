a1ae7b407b8993bf7ec8b97fb1a129355595351b
Jakub Kacalek <kacalek.kuba@post.cz>
2021-11-04 16:32:46 +0100

    get public

--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ const port = 3000
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  //res.send('HelloWorld!')
-  res.sendFile(__dirname + "/../public/index.html");
-})
+// app.get('/', (req, res) => {
+//   //res.send('HelloWorld!')
+// //   res.sendFile(__dirname + "/../public/index.html");
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)

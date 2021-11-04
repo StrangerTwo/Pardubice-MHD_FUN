ac75cf501a3817ab1d2dcba569373e9f815c5aa1
StrangerTwo <h.volhejn@gmail.com>
2021-11-04 16:09:11 +0100

    xd

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   //res.send('HelloWorld!')
-  res.sendFile(__dirname + "/../public/index.htm");
+  res.sendFile(__dirname + "/../public/index.html");
 })
 
 app.listen(port, () => {

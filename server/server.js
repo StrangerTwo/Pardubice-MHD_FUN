222cae84563ba1ff5d6e9daf4519a9fc28546205
StrangerTwo <h.volhejn@gmail.com>
2021-11-04 16:53:09 +0100

    fix?

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 app.use(express.static('../public'));
+app.use(express.static('../public/js'));
 
 // app.get('/', (req, res) => {
 //   //res.send('HelloWorld!')

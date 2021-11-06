6dc61ea0162d11b084b93734d262c2fa2667112a
StrangerTwo <h.volhejn@gmail.com>
2021-11-06 17:12:00 +0100

    scopeeee sw?

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 import Pardubice from './pardubice.js';
 
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('js/sw.js')
+    navigator.serviceWorker.register('js/sw.js', { scope: '/' })
     .then((reg) => console.log("serviceWorker registered", reg))
     .catch((err) => console.log("serviceWorker not registered", err))
 }
